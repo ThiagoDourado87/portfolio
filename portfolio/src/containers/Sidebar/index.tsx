299167f195b0ee7de1d2eb494f45c189e2ac2d1e
ThiagoDourado87 <thiagodourado@gmail.com>
2024-04-16 21:32:30 -0300

    estilizando a sidebar

--- portfolio/src/containers/Sidebar/index.tsx
+++ portfolio/src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ThiagoDourado87.png" />
-    <Titulo fontSize={20}>Thiago Teixeira Dourado</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Thiago Teixeira Dourado</Titulo>
+      <Paragrafo tipo="secundario">thiagodourado87</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
